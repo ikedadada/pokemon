9b81bf2166ad63abb9a469baa8d3d6eb56a76ff8
koichi.ikeda <koichiikeda.0111@gmail.com>
2022-06-14 19:47:54 +0900

    環境変数のチェック

---
+++ pages/api/env.ts
@@ -0,0 +1,14 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import * as fs from 'fs'
+import { BigQuery, JobLoadMetadata } from '@google-cloud/bigquery'
+import credential from '../../gcloud-key.json'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const projectId = process.env.BIGQUERY_PROJECT_ID
+  const datasetId = process.env.BIGQUERY_DATASET_ID
+  res.status(200).json({projectId,datasetId})
+}
